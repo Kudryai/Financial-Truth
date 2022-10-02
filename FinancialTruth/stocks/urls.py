9ad807d25956c323919e64744b005ff64f277940
Kudryavtcev Alexey <st1gii@yahoo.com>
2022-10-02 15:00:10 +0300

    Edit forms

--- FinancialTruth/stocks/urls.py
+++ FinancialTruth/stocks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.StockView.as_view()),
-    path('<slug:slug>/', views.StockDetailView.as_view(), name='stocks_detail')
+    path('<slug:slug>/', views.StockDetailView.as_view(), name='stocks_detail'),
+    path('review/<int:pk>/',views.AddReview.as_view(), name='add_review')
 ]
